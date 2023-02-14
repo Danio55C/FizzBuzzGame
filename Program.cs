5ee71c64ba74bc8944aa0774462f035a205b654e
daniel <jancadaniel55@gmail.com>
2023-02-14 12:30:10 +0100

    walidacja oraz pętla

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,47 +11,77 @@ namespace FizzBuzzGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Gra FizzBuzz, Podaj liczbę: ");
-            int userNumber = int.Parse(Console.ReadLine());
-
             FizzBuzzGame player1 = new FizzBuzzGame();
-            Console.WriteLine(player1.FizzBuzz(userNumber));
+            
+            while (true)
+            {
+                Console.WriteLine("\njeśli chcesz wyjść z porgramu wpisz 'x' oraz zatwierdź enter\nPodaj kolejną liczbę:  ");
+                int userNumber = GetValue();
+                Console.WriteLine(player1.FizzBuzz(userNumber));
+                Console.WriteLine();
+            }
+        }
+
+        private static int GetValue()
+        {
+
+
+            while (true)
+            {
+                var val=Console.ReadLine();
+                if (val.ToUpper()=="X")
+                    Environment.Exit(0);
+
+                if (!int.TryParse(val, out int valid))
+                {
+                    Console.WriteLine("Zła wartość,podaj liczbę. Spróbuj ponownie: ");
+                    continue;
+                }
+                return valid;
+            }
         }
     }
-
-
 
     class FizzBuzzGame
     {
         public FizzBuzzGame()
         {
-            Console.WriteLine("Podaj liczbę: ");
+            Console.WriteLine("Gra FizzBuzz");
         }
+
         public string FizzBuzz(int input)
         {
-
-            if (input % 5 == 0 && input % 3 == 0)
-            {
-                return "Fizz";
-
-            }
-            else if (input % 5 == 0)
-            {
-                return "Buzz";
-
-            }
-            else if (input % 3 == 0)
-            {
+            if ((input % 5 == 0) && (input % 3 == 0))
                 return "FizzBuzz";
-
-            }
+            else if (input % 5 == 0)
+                return "Buzz";
+            else if (input % 3 == 0)
+                return "Fizz";
             else
-            {
                 return input.ToString();
-
-            }
-
-
         }
     }
 }
+
+
+            
+            
+
+            
+
+            
+            
+
+            
+
+
+
+
+
+            
+            
+
+
+
+
+
